27ff972a0bfc3d9e3c063da323a69a47626481ad
Enweave <enweave@gmail.com>
2024-06-18 02:11:44 +0500

    player aim

--- framework/characters/player.gd
+++ framework/characters/player.gd
@@ -1,12 +1,44 @@
 extends wjCharacterBase
 
+var cached_aim_direction : Quaternion = Quaternion()
+var wordlspace : PhysicsDirectSpaceState3D 
+
+
+func _ready():
+    %CameraPivot.rotation_degrees.y = 0
+	
+    wordlspace = get_world_3d().get_direct_space_state()
+
+func get_aim_direction() -> Quaternion:
+    var aim_direction = self.global_transform.basis.get_rotation_quaternion()
+
+    # mouse logic
+    var mouse_position = get_viewport().get_mouse_position()
+    var ray = PhysicsRayQueryParameters3D.new()
+    
+    ray.from = %Camera.project_ray_origin(mouse_position)
+    ray.to = ray.from +  %Camera.project_ray_normal(mouse_position) * 2000
+    
+    var hit_result = wordlspace.intersect_ray(ray)
+    
+    if hit_result:
+        var _hit_position = hit_result.position
+        _hit_position.y = self.global_position.y
+        var _direction = self.global_transform.origin.direction_to(_hit_position)
+        aim_direction = Quaternion(Vector3.FORWARD, _direction)
+		
+    return aim_direction
+
+
 func calc_movement(delta):
     super.calc_movement(delta)
-    # Get the input direction and handle the movement/deceleration.
-    # As good practice, you should replace UI actions with custom gameplay actions.
+    # Set player look direction.
+    self.set_quaternion(get_aim_direction())
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
     var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-    var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-    
+    var direction = (Vector3(input_dir.x, 0, input_dir.y)).normalized()
     if direction:
         velocity.x = direction.x * SPEED
         velocity.z = direction.z * SPEED
---
+++ framework/characters/playerCameraHelper.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+var root
+var LERP_FACTOR = 3
+var OFFSET_FACTOR = 0.5
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	root = get_parent()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var root_pos = root.global_position + root.velocity * OFFSET_FACTOR
+	# interpolate position of camera
+	var new_pos = self.global_position.lerp(root_pos, delta * LERP_FACTOR)
+	# set position of self
+	self.global_position = new_pos
