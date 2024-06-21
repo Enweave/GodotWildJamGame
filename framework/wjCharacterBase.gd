fcd13f9a054f3447764dee33c6ae1c0c2aca3509
Enweave <enweave@gmail.com>
2024-06-22 03:43:05 +0500

    player respawn + begin level design

--- framework/wjCharacterBase.gd
+++ framework/wjCharacterBase.gd
@@ -62,6 +62,8 @@ func update_heading(target: Vector3):
 
 
 func take_damage(damage_amount: float, attacker: wjCharacterBase = null):
+	if is_dead:
+		return
 	health -= damage_amount
 	
 	being_attacked_by.emit(damage_amount, attacker)
@@ -72,8 +74,14 @@ func take_damage(damage_amount: float, attacker: wjCharacterBase = null):
 	if health <= 0:
 		is_dead = true
 		character_died.emit()
-		await get_tree().create_timer(1.0).timeout
-		queue_free()
+		character_body.update_health_display('dead')
+		# await get_tree().create_timer(1.0).timeout
+		# queue_free()
+
+func reset_health():
+	health = max_health
+	is_dead = false
+	character_body.update_health_display(str(health))
 
 
 func use_ability_with_slowdown(ability: wjAbilityBase = null):
--- levels/level2.gd
+++ levels/level2.gd
@@ -1,3 +1,10 @@
 extends wjLevel
 
+@export var player_respawn: Node3D
+@export var player: wjPlayer
 
+func _on_player_character_died():
+    # set timeout
+    await get_tree().create_timer(0.2).timeout
+    player.global_transform.origin = player_respawn.global_transform.origin
+    player.reset_health()
